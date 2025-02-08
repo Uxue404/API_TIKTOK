f8527cba9e554f53be8e0c799b9cb89f14a5ff31
Uxue <thechuchu049@gmail.com>
2025-02-07 19:24:06 -0600

    fix: corrige bugs varios

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from flask_cors import CORS 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 DOWNLOAD_FOLDER = "downloads"
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
